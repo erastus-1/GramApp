33263a03f9511aa89b024b1b68a74b5afd3f7a95
erastus <erastuskariuki15@gmail.com>
2021-01-17 09:29:56 +0300

    added more views

--- gramapp/views.py
+++ gramapp/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     return render(request, 'welcome.html')
+
+def search(request):
+    profiles = User.objects.all()
+
+    if 'username' in request.GET and request.GET['username']:
+        search_term = request.GET.get('username')
+        results = User.objects.filter(username__icontains=search_term)
+        print(searchs)
+
+        return render(request,'search.html',locals())
+
+    return redirect(home)
