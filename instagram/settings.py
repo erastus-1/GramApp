5f9b5c117494ddc5e99467038d6ff6b5cfebc851
erastus <erastuskariuki15@gmail.com>
2021-01-17 14:38:58 +0300

    install tinymce

--- instagram/settings.py
+++ instagram/settings.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'gramapp',
     'bootstrap4',
     'vote',
+    'registration',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- instagram/urls.py
+++ instagram/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('',include('gramapp.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('register', views.LoginView.as_view(template_name='django_registration/registration_form.html')),
-    path('login', views.LoginView.as_view(template_name='django_registration/login.html')),
-    path('accounts/logout/', views.LogoutView.as_view(next_page= '/')),
+    path('accounts/register/', views.LoginView.as_view(template_name='django_registration/registration_form.html')),
+    path('accounts/login/', views.LoginView.as_view(template_name='django_registration/login.html')),
+    path('accounts/logout/',views.LogoutView.as_view(next_page= '/')),
 ]
