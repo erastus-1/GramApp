4287eb11cb72766e8522b98f4f743489c567b25a
erastus <erastuskariuki15@gmail.com>
2021-01-17 11:32:59 +0300

    updated app views

--- gramapp/views.py
+++ gramapp/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.decorators import login_required
 from .models import Image, Profile,Comment,Follow,Likes
 from .forms import ProfileForm,ImageForm,CommentForm
+from django.contrib.auth import views as auth_views
 from django.http  import HttpResponse,Http404
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
