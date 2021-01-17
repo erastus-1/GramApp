8611aa0a4e8fdc37a1e1f6a0a3346f59edfa017e
erastus <erastuskariuki15@gmail.com>
2021-01-17 09:43:24 +0300

    updated views

--- gramapp/views.py
+++ gramapp/views.py
@@ -2,8 +2,31 @@ from django.http  import HttpResponse,Http404
 from django.shortcuts import render
 
 # Create your views here.
-def welcome(request):
-    return render(request, 'welcome.html')
+# @login_required(login_url='/accounts/login/')
+def home(request):
+    current_user = request.user
+    all_images = Image.objects.all()
+    comments = Comment.objects.all()
+    likes = Likes.objects.all
+    profile = Profile.objects.all()
+    print(likes)
+    return render(request,'home.html',locals())
+
+# @login_required(login_url='accounts/login/')
+def add_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            add=form.save(commit=False)
+            add.profile = current_user
+            add.save()
+            return redirect('home')
+    else:
+        form = ImageForm()
+
+
+    return render(request,'image.html',locals())
 
 def search(request):
     profiles = User.objects.all()
@@ -16,3 +39,28 @@ def search(request):
         return render(request,'search.html',locals())
 
     return redirect(home)
+
+def comment(request,image_id):
+    current_user=request.user
+    image = Image.objects.get(id=image_id)
+    profile_owner = User.objects.get(username=current_user)
+    comments = Comment.objects.all()
+    print(comments)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.image = image
+            comment.comment_owner = current_user
+            comment.save()
+
+            print(comments)
+
+
+        return redirect(home)
+
+    else:
+        form = CommentForm()
+
+    return render(request, 'comment.html', locals())
+
