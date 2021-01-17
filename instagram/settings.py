5c0cec4e86a8f52e59dfedf7e4ca29b8f11e3cbe
erastus <erastuskariuki15@gmail.com>
2021-01-17 09:31:17 +0300

    added django-vote

--- instagram/settings.py
+++ instagram/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gramapp',
+    'bootstrap4',
+    'vote',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
