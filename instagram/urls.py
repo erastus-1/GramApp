f4f92fcb6cce11e210dabcc5ced6a201eb231844
erastus <erastuskariuki15@gmail.com>
2021-01-17 11:33:35 +0300

    updated main urls

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path,include
+from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gramapp/',include('gramapp.urls')),
+    path('',include('gramapp.urls')),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/logout/$', views.logout_then_login, {"next_page": '/'}),
 ]
