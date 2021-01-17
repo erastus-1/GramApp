aebf7c015fc9dc1b8a4e28615d853740874ee8ab
erastus <erastuskariuki15@gmail.com>
2021-01-17 10:05:08 +0300

    updated app urls

--- gramapp/urls.py
+++ gramapp/urls.py
@@ -6,6 +6,11 @@ from . import views
 urlpatterns=[
     url('^$',views.home,name = 'home'),
     url('search/', views.search, name='search')
+    url(r'^image/$', views.add_image, name='upload_image'),
+    url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
+    url(r'^follow/(?P<user_id>\d+)', views.follow, name = 'follow'),
+    url(r'^unfollow/(?P<user_id>\d+)', views.unfollow, name='unfollow'),
+    url(r'^like/(\d+)/$', views.like_images,name='like')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
