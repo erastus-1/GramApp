0fd72b68837d44b178f6f94ee9559a23a62f315c
erastus <erastuskariuki15@gmail.com>
2021-01-17 08:16:36 +0300

    updated search url

--- gramapp/urls.py
+++ gramapp/urls.py
@@ -1,6 +1,11 @@
+from django.conf.urls.static import static
+from django.conf import setting
 from django.conf.urls import url
 from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
+    url('search/', views.search, name='search')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
